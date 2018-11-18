721d1c3d6f44cc1ff1e173622ba8eb18fecd3dc4
Bruno Assis <bfassis@gmail.com>
2018-11-17 23:55:18 -0200

    Commit Banco

---
+++ TesteStefanini/TesteStefaniniDB/Cliente.sql
@@ -0,0 +1,18 @@
+﻿
+
+CREATE TABLE [dbo].[Cliente](
+	[Codigo] [int] NOT NULL,
+	[Nome] [varchar](20) NOT NULL,
+	[Sobrenome] [varchar](30) NOT NULL,
+	[DataNascimento] [date] NOT NULL,
+	[Sexo] [varchar](20) NOT NULL,
+	[Email] [varchar](30) NOT NULL,
+	[Ativo] [bit] NOT NULL,
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
+(
+	[Codigo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
---
+++ TesteStefanini/TesteStefaniniDB/VinculoClienteProduto.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[VinculoClienteProduto](
+	[Codigo] [int] IDENTITY(1,1) NOT NULL,
+	[CodigoProduto] [int] NOT NULL,
+	[CodigoCliente] [int] NOT NULL,
+ CONSTRAINT [PK_VinculoClienteProduto] PRIMARY KEY CLUSTERED 
+(
+	[Codigo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[VinculoClienteProduto]  WITH CHECK ADD  CONSTRAINT [FK_VinculoClienteProduto_Cliente] FOREIGN KEY([CodigoCliente])
+REFERENCES [dbo].[Cliente] ([Codigo])
+GO
+
+ALTER TABLE [dbo].[VinculoClienteProduto] CHECK CONSTRAINT [FK_VinculoClienteProduto_Cliente]
+GO
+
+ALTER TABLE [dbo].[VinculoClienteProduto]  WITH CHECK ADD  CONSTRAINT [FK_VinculoClienteProduto_Produto] FOREIGN KEY([CodigoProduto])
+REFERENCES [dbo].[Produto] ([Codigo])
+GO
+
+ALTER TABLE [dbo].[VinculoClienteProduto] CHECK CONSTRAINT [FK_VinculoClienteProduto_Produto]
+GO
+
+
